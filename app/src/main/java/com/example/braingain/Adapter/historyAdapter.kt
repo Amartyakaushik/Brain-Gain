266a7c0fbdc099fa440a11dc8c645adf9f6ba619
Amartya <kamartya58@gmail.com>
2024-11-21 11:26:33 +0530

    Updated history fragment where reflected as the money transaction status. 1) Now the coin will be shown with a minus sign if money is debited and will shown with a plus sign if money is credited. 2) withdrawal status will be shown as diffent images (one for credit and one for debit).

--- app/src/main/java/com/example/braingain/Adapter/historyAdapter.kt
+++ app/src/main/java/com/example/braingain/Adapter/historyAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.braingain.ModelClass.historyModelClass
+import com.example.braingain.R
 import com.example.braingain.databinding.HistoryItemListBinding
 import java.sql.Date
 import java.sql.Timestamp
@@ -24,16 +25,22 @@ class historyAdapter(var historyList : ArrayList<historyModelClass>) :
         // for showing exact when the amount is credited
         var timeStamp = Timestamp(historyList.get(position).timeAndDate.toLong())
         holder.binding.time.text = Date(timeStamp.time).toString()
+
         // to inform if the money is debited or credited
         if(historyList.get(position).isWithdrawal) {
             holder.binding.status.text = "Debited"
-            holder.binding.status.setTextColor(Color.RED)
+//            holder.binding.status.setTextColor(Color.RED)
+        holder.binding.coinCount.text = "- " + historyList[position].coin
+            holder.binding.coinCount.setTextColor(Color.RED)
+            holder.binding.withdrawalStatus.setImageResource(R.drawable.ic_debit)
 
         }else{
             holder.binding.status.text = "Credited"
-            holder.binding.status.setTextColor(Color.GREEN)
+//            holder.binding.status.setTextColor(Color.GREEN)
+        holder.binding.coinCount.text = "-" + historyList[position].coin
+            holder.binding.coinCount.setTextColor(Color.GREEN)
+            holder.binding.withdrawalStatus.setImageResource(R.drawable.ic_credit)
         }
-        holder.binding.coinCount.text = historyList[position].coin
 
     }
 }
